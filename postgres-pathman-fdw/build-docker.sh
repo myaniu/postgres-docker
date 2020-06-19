40f596665ff4b3e652e5ebbc0dcdc9d3110927de
李飞 <27982221@qq.com>
2020-06-19 10:49:52 +0800

    delete unuse cmd

--- postgres-pathman-fdw/build-docker.sh
+++ postgres-pathman-fdw/build-docker.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #!/bin/sh
 VERSION=12.3
-docker build -t myaniu/library/postgres-pathman-fdw:$VERSION .
-docker tag myaniu/library/postgres-pathman-fdw:$VERSION myaniu/library/postgres-pathman-fdw:latest
+docker build -t myaniu/postgres-pathman-fdw:$VERSION .
+docker tag myaniu/postgres-pathman-fdw:$VERSION myaniu/postgres-pathman-fdw:latest
 
 
-docker push myaniu/library/postgres-pathman-fdw:$VERSION
-docker push myaniu/library/postgres-pathman-fdw:latest
+docker push myaniu/postgres-pathman-fdw:$VERSION
+docker push myaniu/postgres-pathman-fdw:latest
